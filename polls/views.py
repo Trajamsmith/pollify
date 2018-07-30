a4abd49592b0881c19616c51cf58c32fdd45d1b0
Travis James Smith <trajamsmith@gmail.com>
2018-07-30 16:50:01 -0500

    Fix view rendering bug

--- polls/views.py
+++ polls/views.py
@@ -23,7 +23,7 @@ class DetailView(generic.DetailView):
 
 class ResultsView(generic.DetailView):
     model = Question
-    template_name = 'polls/details.html'
+    template_name = 'polls/results.html'
 
 
 def vote(request, question_id):
